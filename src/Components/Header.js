1d04f64a8eb3e1cb9c77e8f10c209b561c0432c6
gautamV19 <ee20b144@smail.iitm.ac.in>
2023-12-14 19:16:24 +0530

    Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,79 @@
-import React from 'react'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../Assets/Images/logo.png";
 
-export default function Header() {
+
+const pages = ["Home", "About", "Events", "People"];
+
+function Header() {
+    const navigate = useNavigate();
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
-        <div>Header</div>
-    )
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar >
+                    <Box sx={{ flexGrow: 0 }}>
+                        <IconButton
+                            onClick={handleOpenUserMenu}
+                            sx={{ p: 0 }}
+                            onClickCapture={() => {
+                                navigate("/");
+                            }}
+                        >
+                            <img src={logo} alt="logo" width="150px" height="80px" />
+                        </IconButton>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={() => {
+                                    (page === "Home") ? navigate("/") : navigate(`/${page.toLowerCase()}`);
+                                }}
+                                sx={{ my: 2, color: "white", display: "block" }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Button variant="contained" onClick={() => {
+                        navigate('/join');
+                    }}>
+                        Join
+                    </Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
+export default Header;
