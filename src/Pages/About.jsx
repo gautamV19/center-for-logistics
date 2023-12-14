57001917f9e66702a4b20744cb8b770ecc35f86c
gautamV19 <ee20b144@smail.iitm.ac.in>
2023-12-14 23:27:29 +0530

    vision

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,108 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <Grid
+      container
+      spacing={1}
+      direction={"column"}
+      justify="center"
+      alignItems="center"
+    >
+      <Grid
+        item
+        container
+        justify="center"
+        alignItems="center"
+        direction={"column"}
+      >
+        <Typography variant="h2">Vision</Typography>
+        <Typography variant="h4" sx={{ width: "80%", textAlign: "center" }}>
+          To build a centre which fosters innovation and enables environmental
+          and social sustainability in logistics and supply chain with
+          cutting-edge digital transformation technologies.
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={"column"}
+        alignItems="flex-start"
+        justifyContent={"center"}
+        spacing={0}
+        width={"50%"}
+      >
+        <Grid item>
+          <Typography variant="h5" mt={5}>
+            Net Zero Carbon + Happy, Safe and Comfortable Workplace
+          </Typography>
+          <List>
+            <ListItem>
+              <Typography variant="h6">
+                Harmony among logistics service providers - 100% resource
+                utilization
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="h6">
+                Increase reliability of logistics services
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="h6">
+                Increase overall satisfaction in the ecosystem - service
+                providers and consumers
+              </Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5" mt={5}>
+            Digital transformation for end-to-end supply chain solutions and
+            decision making
+          </Typography>
+          <List>
+            <ListItem>
+              <Typography variant="h6">
+                Using IoT - to gather dynamic information
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography variant="h6">
+                Algorithmic and Machine Learning
+              </Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5" mt={5}>
+            Global network of experts
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5" mt={5}>
+            A resource centre for technology and processes in logistics
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5" mt={5}>
+            High visibility, productivity (scientific publications), large
+            outreach (seminars, workshops, conferences, summer programs),
+            conference proceedings
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Typography variant="h2">Focus</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="h2">Impact</Typography>
+      </Grid>
+    </Grid>
+  );
 }
